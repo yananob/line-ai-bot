7792b89d5fe949700c663d7a21355d6d13a9abdd
yananob <yananob@gmail.com>
2024-11-30 18:54:56 +0900

    1st build

--- index.php
+++ index.php
@@ -7,37 +7,16 @@ use Psr\Http\Message\ServerRequestInterface;
 use Google\CloudFunctions\FunctionsFramework;
 use GuzzleHttp\Psr7\Response;
 use yananob\mytools\Logger;
-use yananob\mytools\Utils;
+// use yananob\mytools\Utils;
 use yananob\mytools\Line;
-use yananob\mytools\Gpt;
-
-const GPT_CONTEXT = <<<EOM
-<bot/characteristics>
-
-カウンセリング相手の情報：
-<human/characteristics>
-
-依頼事項：
-カウンセリング相手からのメッセージに対して、カウンセリング相手の特徴を一部反映して、ポジティブなフィードバックを、300〜500字ぐらいで返してください。
-EOM;
-// TODO: 「依頼事項」もConfig化
-
-function getContext($config): string
-{
-    $result = GPT_CONTEXT;
-    $replaceSettings = [
-        ["search" => "<bot/characteristics>", "replace" => $config->bot->characteristics],
-        ["search" => "<human/characteristics>", "replace" => $config->human->characteristics],
-    ];
-    foreach ($replaceSettings as $replaceSetting) {
-        $result = str_replace($replaceSetting["search"], $replaceSetting["replace"], $result);
-    }
-    return $result;
-}
+use MyApp\LineWebhookMessage;
+use MyApp\PersonalConsultant;
 
 FunctionsFramework::http('main', 'main');
 function main(ServerRequestInterface $request): ResponseInterface
 {
+    // $config = Utils::getConfig(__DIR__ . "/configs/config.json", asArray: false);
+
     $logger = new Logger("webhook-receive");
     $logger->log(str_repeat("-", 120));
     $logger->log("headers: " . json_encode($request->getHeaders()));
@@ -45,10 +24,6 @@ function main(ServerRequestInterface $request): ResponseInterface
     $logger->log("parsedBody: " . json_encode($request->getParsedBody()));
     $body = $request->getBody()->getContents();
     $logger->log("body: " . $body);
-    $body = json_decode($body, false);
-
-    $event = $body->events[0];
-    $message = $event->message->text;
 
     /** 
      * 1. LINE webhook受ける
@@ -58,35 +33,25 @@ function main(ServerRequestInterface $request): ResponseInterface
      * 5. メッセージをLINEで送る
      */
 
-    $config = Utils::getConfig(__DIR__ . "/configs/config.json", asArray: false);
+    $webhookMessage = new LineWebhookMessage($body);
 
-    // TODO: LINE Webhookから来たデータを処理するラッパーがあったほうがよさそう
-    $type = $event->source->type;
-    $targetId = null;
-    // typeを判定して、idを取得
-    if ($type === 'user') {
-        $targetId = $event->source->userId;
-    } else if ($type === 'group') {
-        $targetId = $event->source->groupId;
-    } else if ($type === 'room') {
-        $targetId = $event->source->roomId;
-    } else {
-        throw new Exception("Unknown type :" + $type);
-    }
-
-    $gpt = new Gpt(__DIR__ . "/configs/gpt.json");
-    $answer = $gpt->getAnswer(
-        context: getContext($config),
-        message: $message,
-    );
+    $consultant = new PersonalConsultant($webhookMessage->getTargetId());
 
     $line = new Line(__DIR__ . "/configs/line.json");
     $line->sendMessage(
-        bot: $config->bot->line_target,
-        targetId: $targetId,
-        message: $answer,
-        replyToken: $event->replyToken
+        bot: $consultant->getLineTarget(),
+        targetId: $webhookMessage->getTargetId(),
+        message: $consultant->getAnswer($webhookMessage->getMessage()),
+        replyToken: $webhookMessage->getReplyToken(),
     );
+
+    // $line = new Line(__DIR__ . "/configs/line.json");
+    // $line->sendMessage(
+    //     bot: $config->bot->line_target,
+    //     targetId: $targetId,
+    //     message: $answer,
+    //     replyToken: $event->replyToken
+    // );
   
     $headers = ['Content-Type' => 'application/json'];
     return new Response(200, $headers, json_encode($body));
---
+++ src/PersonalConsultant.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyApp;
+
+use TargetNotDefinedException;
+use yananob\mytools\Utils;
+use yananob\mytools\Gpt;
+
+class PersonalConsultant
+{
+    private Gpt $gpt;
+    private object $config;
+
+    const GPT_CONTEXT = <<<EOM
+<bot/characteristics>
+
+カウンセリング相手の情報：
+<human/characteristics>
+
+依頼事項：
+<request>
+EOM;
+
+    public function __construct(private string $targetId)
+    {
+        $this->gpt = new Gpt(__DIR__ . "/../configs/gpt.json");
+
+        $config = Utils::getConfig(__DIR__ . "/../config.json");
+        if (!array_key_exists($targetId, $config)) {
+            throw new TargetNotDefinedException("targetId [{$targetId}] is not defined.");
+        }
+
+        $this->config = json_decode($config[$targetId], false);
+    }
+
+    public function getAnswer(string $question): string
+    {
+        return $this->gpt->getAnswer(
+            context: $this->__getContext(),
+            message: $question,
+        );
+    }
+
+    private function __getContext(): string
+    {
+        $result = self::GPT_CONTEXT;
+        $replaceSettings = [
+            ["search" => "<bot/characteristics>", "replace" => $this->config->bot->characteristics],
+            ["search" => "<human/characteristics>", "replace" => $this->config->human->characteristics],
+            ["search" => "<request>", "replace" => $this->config->request],
+        ];
+        foreach ($replaceSettings as $replaceSetting) {
+            $result = str_replace($replaceSetting["search"], $replaceSetting["replace"], $result);
+        }
+        return $result;
+    }
+
+    public function getLineTarget(): string
+    {
+        return $this->config->bot->line_target;
+    }
+}
