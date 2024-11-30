fcb5d20b84c4980dc57ccd455532c687344c9ac5
yananob <yananob@gmail.com>
2024-11-30 20:04:09 +0900

    creating tests

--- tests/PersonalConsultantTest.php
+++ tests/PersonalConsultantTest.php
@@ -13,6 +13,11 @@ final class PersonalConsultantTest extends PHPUnit\Framework\TestCase
         $this->consultant = new PersonalConsultant(__DIR__ . "/configs/config.json", "TEST_TARGET_ID");
     }
 
+    public function testGetAnswer()
+    {
+        $this->assertNotEmpty($this->consultant->getAnswer("今年のクリスマスは何月何日でしょうか？\n昨年のクリスマスとは違うのでしょうか？"));
+    }
+
     public function testGetLineTarget()
     {
         $this->assertSame("TEST_LINE_TARGET", $this->consultant->getLineTarget());
