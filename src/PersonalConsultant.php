c7759307c121f410dfe9a19eaa7ca596c76e0d8a
yananob <yananob@gmail.com>
2024-12-21 13:23:48 +0900

    #6-support-nonregisterd-start (#11)
    * config.jsonに登録しなくても動くようにしたい #6
    
    * config.jsonに登録しなくても動くようにしたい #6
    
    * config.jsonに登録しなくても動くようにしたい #6
    
    * config.jsonに登録しなくても動くようにしたい #6
    
    * config.jsonに登録しなくても動くようにしたい #6
    
    * config.jsonに登録しなくても動くようにしたい #6

--- src/PersonalConsultant.php
+++ src/PersonalConsultant.php
@@ -18,7 +18,7 @@ class PersonalConsultant
     const GPT_CONTEXT = <<<EOM
 <bot/characteristics>
 
-【カウンセリング相手の情報】
+<title/human_characteristics>
 <human/characteristics>
 
 <title/recentConversations>
@@ -34,10 +34,11 @@ EOM;
         $this->conversations = new Conversations($targetId, $isTest);
 
         $config = Utils::getConfig($configPath, false);
-        if (!property_exists($config, $targetId)) {
-            throw new TargetNotDefinedException("targetId [{$targetId}] is not defined.");
+        if (property_exists($config, $targetId)) {
+            $this->config = $config->$targetId;
+        } else {
+            $this->config = $config->default;
         }
-        $this->config = $config->$targetId;
     }
 
     public function getAnswer(bool $applyRecentConversations, string $message): string
@@ -58,27 +59,35 @@ EOM;
         $result = self::GPT_CONTEXT;
         $replaceSettings = [
             ["search" => "<bot/characteristics>", "replace" => $this->config->bot->characteristics],
-            ["search" => "<human/characteristics>", "replace" => $this->config->human->characteristics],
             ["search" => "<request>", "replace" => $this->config->request],
         ];
         foreach ($replaceSettings as $replaceSetting) {
             $result = str_replace($replaceSetting["search"], $replaceSetting["replace"], $result);
         }
 
+        if (empty($this->config->human)) {
+            $result = $this->__removeFromContext(["<title/human_characteristics>", "<human/characteristics>"], $result);
+        } else {
+            $result = str_replace("<title/human_characteristics>", "【話し相手の情報】", $result);
+            $result = str_replace("<human/characteristics>", $this->config->human->characteristics, $result);
+        }
+
         if (empty($conversations)) {
-            foreach (["<title/recentConversations>", "<recentConversations>"] as $replaceKey) {
-                $result = str_replace($replaceKey . "\n", "", $result);
-            }
+            $result = $this->__removeFromContext(["<title/recentConversations>", "<recentConversations>"], $result);
         } else {
             $result = str_replace("<title/recentConversations>", "【最近の会話内容】", $result);
-            $result = str_replace(
-                "<recentConversations>",
-                $this->__convertConversationsToText($conversations),
-                $result
-            );
+            $result = str_replace("<recentConversations>", $this->__convertConversationsToText($conversations), $result);
         }
 
         return $result;
+    }
+
+    private function __removeFromContext(array $keywords, string $source): string
+    {
+        foreach ($keywords as $keyword) {
+            $source = str_replace($keyword . "\n", "", $source);
+        }
+        return $source;
     }
 
     private function __convertConversationsToText(array $conversations): string
@@ -86,7 +95,7 @@ EOM;
         $result = "";
         foreach ($conversations as $conversation) {
             $result .= "・日時：" . $conversation->created_at . "\n";
-            $result .= "・発言者：" . ($conversation->by === "human" ? "会話相手" : "ボット（あなた）") . "\n";
+            $result .= "・発言者：" . ($conversation->by === "human" ? "話し相手" : "チャットボット（あなた）") . "\n";
             $result .= "・内容：" . $conversation->content . "\n";
             $result .= str_repeat("-", 80) . "\n";
         }
--- tests/PersonalConsultantTest.php
+++ tests/PersonalConsultantTest.php
@@ -2,29 +2,90 @@
 
 declare(strict_types=1);
 
+use Carbon\Carbon;
 use MyApp\PersonalConsultant;
 
 final class PersonalConsultantTest extends PHPUnit\Framework\TestCase
 {
-    private PersonalConsultant $consultant;
+    private PersonalConsultant $consultant_defined;
+    private PersonalConsultant $consultant_undefined;
 
     protected function setUp(): void
     {
-        $this->consultant = new PersonalConsultant(__DIR__ . "/configs/config.json", "TEST_TARGET_ID");
+        $this->consultant_defined = new PersonalConsultant(__DIR__ . "/configs/config.json", "TARGET_ID_TEST");
+        $this->consultant_undefined = new PersonalConsultant(__DIR__ . "/configs/config.json", "TARGET_ID_NOT_EXISTS");
+    }
+
+    private function __invokePrivateMethod($object, string $methodName, array $params): mixed
+    {
+        $reflection = new \ReflectionClass($object);
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invoke($object, $params);
     }
 
     public function testGetAnswerWithoutRecentConversation()
     {
-        $this->assertNotEmpty($this->consultant->getAnswer(false, "今年のクリスマスは何月何日でしょうか？\n昨年のクリスマスとは違うのでしょうか？"));
+        $this->assertNotEmpty($this->consultant_defined->getAnswer(
+            false,
+            "今年のクリスマスは何月何日でしょうか？\n昨年のクリスマスとは違うのでしょうか？"
+        ));
     }
 
     public function testGetAnswerWithRecentConversation()
     {
-        $this->assertNotEmpty($this->consultant->getAnswer(true, "今年のクリスマスは何月何日でしょうか？\n昨年のクリスマスとは違うのでしょうか？"));
+        $this->assertNotEmpty($this->consultant_defined->getAnswer(
+            true,
+            "今年のクリスマスは何月何日でしょうか？\n昨年のクリスマスとは違うのでしょうか？"
+        ));
     }
 
-    public function testGetLineTarget()
+    public function testGetContext_WithOutTargetConfiguration()
     {
-        $this->assertSame("TEST_LINE_TARGET", $this->consultant->getLineTarget());
+        $this->assertStringNotContainsString(
+            "【話し相手の情報】",
+            $this->__invokePrivateMethod($this->consultant_undefined, "__getContext", [])
+        );
+    }
+
+    public function testGetContext_WithTargetConfiguration()
+    {
+        $this->assertStringContainsString(
+            "【話し相手の情報】",
+            $this->__invokePrivateMethod($this->consultant_defined, "__getContext", [])
+        );
+    }
+
+    public function testGetContext_WithoutRecentConversation()
+    {
+        $this->assertStringNotContainsString(
+            "【最近の会話内容】",
+            $this->__invokePrivateMethod($this->consultant_defined, "__getContext", [])
+        );
+    }
+
+    public function testGetContext_WithRecentConversation()
+    {
+        $recentConversations = [];
+        $obj = new stdClass();
+        $obj->by = "human";
+        $obj->content = "今日は旅行に行きました";
+        $obj->created_at = new Carbon("today");
+        $recentConversations[] = $obj;
+
+        $this->assertStringContainsString(
+            "【最近の会話内容】",
+            $this->__invokePrivateMethod($this->consultant_defined, "__getContext", $recentConversations)
+        );
+    }
+
+    public function testGetLineTarget_WithTargetConfiguration()
+    {
+        $this->assertSame("LINE_TARGET_TEST", $this->consultant_defined->getLineTarget());
+    }
+
+    public function testGetLineTarget_WithOutTargetConfiguration()
+    {
+        $this->assertSame("LINE_TARGET_DEFAULT", $this->consultant_undefined->getLineTarget());
     }
 }
