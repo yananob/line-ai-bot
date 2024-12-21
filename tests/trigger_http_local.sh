2f30255879174d933364b4a2201575eef2c1341e
yananob <yananob@gmail.com>
2024-12-21 13:23:27 +0900

    config.jsonに登録しなくても動くようにしたい #6

--- tests/trigger_http_local.sh
+++ tests/trigger_http_local.sh
@@ -3,5 +3,5 @@ set -eu
 
 curl -X POST \
     -H "context-type: application:json" \
-    -d '{"events": [{"source": {"type": "group", "groupId": "TARGET_ID_TEST_ZZ"}, "message": {"text": "今日はとても疲れた！"}, "replyToken": "REPLY_TOKEN"}]}' \
+    -d '{"events": [{"source": {"type": "group", "groupId": "TARGET_ID_TEST"}, "message": {"text": "今日はとても疲れた！"}, "replyToken": "REPLY_TOKEN"}]}' \
     http://localhost:8080
