c276054ae135ea5a79ff7f4c19e75460de15ecfc
yananob <yananob@gmail.com>
2024-12-22 23:04:12 +0900

    draft

--- src/BotConfig.php
+++ src/BotConfig.php
@@ -4,21 +4,47 @@ declare(strict_types=1);
 
 namespace MyApp;
 
+use Google\Cloud\Firestore\CollectionReference;
+use Google\Cloud\Firestore\DocumentReference;
+use Google\Cloud\Firestore\DocumentSnapshot;
+
 class BotConfig
 {
+    private DocumentSnapshot $configGenerated;
+    // private DocumentReference $triggersGenerated;
+    private string $mode;
 
-    public function __construct() {}
+    public function __construct(CollectionReference $collectionReference, private ?BotConfig $configDefault)
+    {
+        $this->configGenerated = $collectionReference->document("config-generated")->snapshot()->data();
+        $this->mode = $this->configGenerated["mode"];
+    }
+
+    private function __getConfig(string $fieldName, bool $useDefaultAndGenerated): array
+    {
+        // $result = [];
+        $result = $this->configGenerated[$fieldName];
+        if ((empty($generated) || $useDefaultAndGenerated) && !empty($configDefault)) {
+            array_push($result, $this->configDefault[$fieldName]);
+        }
+        return $result;
+    }
 
     public function getBotCharacteristics(): array
     {
-        // TODO: default + generatedでの内容を返す
-        return [];
+        return $this->__getConfig("bot_characteristics", false);
     }
-    public function getHumanCharacteristics(): array {}
-
+    public function getHumanCharacteristics(): array
+    {
+        return $this->__getConfig("human_characteristics", false);
+    }
     public function hasHumanCharacteristics(): bool
     {
         return (!empty($this->getHumanCharacteristics()));
+    }
+    public function getRequests(): array
+    {
+        return $this->__getConfig("requests", true);
     }
 
     public function isChatMode(): bool
@@ -31,5 +57,8 @@ class BotConfig
         return $this->mode === Mode::Consulting->value;
     }
 
-    public function getLineTarget(): string {}
+    public function getLineTarget(): string
+    {
+        return $this->configDefault["line_target"];
+    }
 }
--- src/BotConfigsStore.php
+++ src/BotConfigsStore.php
@@ -4,13 +4,35 @@ declare(strict_types=1);
 
 namespace MyApp;
 
+use Google\Cloud\Firestore\FirestoreClient;
+use Google\Cloud\Firestore\DocumentReference;
+
 class BotConfigsStore
 {
+    private FirestoreClient $dbAccessor;
+    private DocumentReference $documentRoot;
 
-    public function __construct() {}
+    public function __construct(bool $isTest = true)
+    {
+        $this->dbAccessor = new FirestoreClient(["keyFilePath" => __DIR__ . '/../configs/firebase.json']);
+        $collectionName = "ai-bot";
+        if ($isTest) {
+            $collectionName .= "-test";
+        }
+        $this->documentRoot = $this->dbAccessor->collection($collectionName)->document("configs");
+    }
 
-    public function exists(string $targetId): bool {}
+    public function exists(string $targetId): bool
+    {
+        return !empty($this->get($targetId));
+    }
 
-    public function get(string $targetId): BotConfig {}
-    public function getDefault(): BotConfig {}
+    public function get(string $targetId): BotConfig
+    {
+        return new BotConfig($this->documentRoot->collection($targetId), $this->getDefault());
+    }
+    public function getDefault(): BotConfig
+    {
+        return new BotConfig($this->documentRoot->collection("default"), null);
+    }
 }
