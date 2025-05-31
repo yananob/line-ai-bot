5e7cab3bb8dcc72957e929c52c8c01dde1d3b175
yananob <yananob@gmail.com>
2025-05-31 23:21:23 +0900

    secret管理　google secret managerに #51

--- tests/listen_local_http.sh
+++ tests/listen_local_http.sh
@@ -3,13 +3,15 @@ set -eu
 
 export FUNCTION_TARGET=main
 
-export OPENAI_KEY_LINE_AI_BOT=$(gcloud secrets versions access latest --secret=OPENAI_KEY_LINE_AI_BOT)
-export LINE_TOKENS_N_TARGETS=$(gcloud secrets versions access latest --secret=LINE_TOKENS_N_TARGETS)
-export FIREBASE_CONFIG=$(gcloud secrets versions access latest --secret=FIREBASE_CONFIG)
-# echo $FIREBASE_CONFIG
+SECRETS=("OPENAI_KEY_LINE_AI_BOT" "LINE_TOKENS_N_TARGETS" "FIREBASE_CONFIG")
+
+for secret in "${SECRETS[@]}"; do
+    export "$secret"="$(gcloud secrets versions access latest --secret="$secret")"
+    # echo "$secret: ${!secret}"
+done
 
 composer start
 
-unset OPENAI_KEY_LINE_AI_BOT
-unset LINE_TOKENS_N_TARGETS
-unset FIREBASE_CONFIG
+for secret in "${SECRETS[@]}"; do
+    unset "$secret"
+done
