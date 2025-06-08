f2deb1588ba623642b08019c449269c85a787d88
yananob <yananob@gmail.com>
2025-06-08 15:54:07 +0900

    shell

--- tests/listen_local_http.sh
+++ tests/listen_local_http.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -eu
 
-export FUNCTION_TARGET=main
-
+# Export credentials
 SECRETS=("OPENAI_KEY_LINE_AI_BOT" "LINE_TOKENS_N_TARGETS" "FIREBASE_CONFIG")
-
 for secret in "${SECRETS[@]}"; do
     export "$secret"="$(gcloud secrets versions access latest --secret="$secret")"
     # echo "$secret: ${!secret}"
 done
 
+# Launch function
+export FUNCTION_TARGET=main
 composer start
 
+# Remove credentials
 for secret in "${SECRETS[@]}"; do
     unset "$secret"
 done
